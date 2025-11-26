37527c34d51b141fcc275a5e3447c3b1c80b316f
Marin Maulbon <marin.maulbon@gmail.com>
2025-11-26 15:14:43 +0100

    added to github pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,7 +1,7 @@
 import os
 from block import markdown_to_html_node, extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     f_content = open(from_path)
     content = f_content.read()
@@ -11,22 +11,24 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_content)
+    template = template.replace('href="/', f"href=\"{basepath}")
+    template = template.replace('src="/', f"src=\"{basepath}")
     with open (dest_path, "w") as f:
         f.write(template)
     f_content.close()
     f_template.close()
 
-def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursively(dir_path_content, template_path, dest_dir_path, basepath):
     all_content = os.listdir(dir_path_content)
     for content in all_content:
         joined_content_path = os.path.join(dir_path_content, content)
         if content.endswith(".md"):
             final_html_file = f"{content.strip('.md')}.html"
             final_dest = os.path.join(dest_dir_path, final_html_file)
-            generate_page(joined_content_path, template_path, final_dest)
+            generate_page(joined_content_path, template_path, final_dest, basepath)
         elif os.path.isdir(joined_content_path):
             final_dir = os.path.join(dest_dir_path, content)
             os.mkdir(final_dir)
-            generate_pages_recursively(joined_content_path, template_path, final_dir)
+            generate_pages_recursively(joined_content_path, template_path, final_dir, basepath)
         else:
             continue
--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from generate_page import generate_pages_recursively
 from copy_to_public import copy_conetent
+import sys
 
 def main ():
-    copy_conetent("./static", "./public")
-    generate_pages_recursively("content", "template.html", "public")
+    if len(sys.argv) > 2:
+        basepath = sys.argv
+    else:
+        basepath = "/"
+    copy_conetent("./static", "./docs")
+    generate_pages_recursively("content", "template.html", "docs", basepath)
 
 main()
 
