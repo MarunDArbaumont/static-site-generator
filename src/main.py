10290e98c9f5cc76f840c8b227cb2a772954f6a0
Marin Maulbon <marin.maulbon@gmail.com>
2025-11-26 15:18:39 +0100

    fix bugs for github pages

--- src/main.py
+++ src/main.py
@@ -2,13 +2,18 @@ from generate_page import generate_pages_recursively
 from copy_to_public import copy_conetent
 import sys
 
+dir_path_static = "./static"
+dir_path_public = "./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
+default_basepath = "/"
+
 def main ():
-    if len(sys.argv) > 2:
-        basepath = sys.argv
-    else:
-        basepath = "/"
-    copy_conetent("./static", "./docs")
-    generate_pages_recursively("content", "template.html", "docs", basepath)
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    copy_conetent(dir_path_static, dir_path_public)
+    generate_pages_recursively(dir_path_content, template_path, dir_path_public, basepath)
 
 main()
 
